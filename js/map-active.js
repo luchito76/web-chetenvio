1512388c71fb1485750b197bd75ac4ad0acc91ce
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-11-07 01:23:31 -0300

    Se agrega google map

--- js/map-active.js
+++ js/map-active.js
@@ -1,40 +1,67 @@
 var map;
-var latlng = new google.maps.LatLng(40.730610, -73.935242);
-var stylez = [{
-    featureType: "all",
-    elementType: "all",
-    stylers: [{
-        saturation: -10
-            }]
-        }];
+const fenway = { lat: 40.73061, lng: -73.935242 };
+var latlng = new google.maps.LatLng(40.73061, -73.935242);
+var stylez = [
+	{
+		featureType: 'all',
+		elementType: 'all',
+		stylers: [
+			{
+				saturation: -10
+			}
+		]
+	}
+];
 var mapOptions = {
-    zoom: 15,
-    center: latlng,
-    scrollwheel: false,
-    scaleControl: false,
-    disableDefaultUI: true,
-    mapTypeControlOptions: {
-        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
-    }
+	zoom: 16,
+	center: latlng,
+	scrollwheel: true,
+	scaleControl: true,
+    disableDefaultUI: false,    
+	mapTypeControlOptions: {
+		mapTypeIds: 'terrain' //[ google.maps.MapTypeId.ROADMAP, 'gMap' ]
+	}
 };
-map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 var geocoder_map = new google.maps.Geocoder();
-var address = 'New york';
-geocoder_map.geocode({
-    'address': address
-}, function (results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: map.getCenter()
-        });
-    } else {
-        alert("Geocode was not successful for the following reason: " + status);
-    }
-});
+var address = 'Nuequen';
+
+geocoder_map.geocode(
+	{
+		address: address
+	},
+	function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+				map: map,				
+				position: map.getCenter()
+			});
+
+			const flightPlanCoordinates = [
+				{ lat: -38.947544, lng: -68.062061 },
+				{ lat: -38.947586, lng: -68.056149 },
+				{ lat: -38.952949, lng: -68.054837 },
+				{ lat: -38.954997, lng: -68.067739 },
+				{ lat: -38.947544, lng: -68.062061 }
+			];
+
+			const bermudaTriangle = new google.maps.Polygon({
+				paths: flightPlanCoordinates,
+				strokeColor: '#FF0000',
+				strokeOpacity: 0.8,
+				strokeWeight: 2,
+				fillColor: '#FF0000',
+				fillOpacity: 0.35
+			});
+			bermudaTriangle.setMap(map);
+		} else {
+			alert('Geocode was not successful for the following reason: ' + status);
+		}
+	}
+);
 var mapType = new google.maps.StyledMapType(stylez, {
-    name: "Grayscale"
+	name: 'Grayscale'
 });
 map.mapTypes.set('gMap', mapType);
 map.setMapTypeId('gMap');
